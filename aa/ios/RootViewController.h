7a63aeddc77d21d1023efa0d277cf73c0fba0029
dong-jinliang <dong-jinliang@meyougames.com>
2013-04-10 10:03:35 +0800

    Initial Commit

---
+++ aa/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  aaAppController.h
+//  aa
+//
+//  Created by MeYouGamesP5 on 13-4-10.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
